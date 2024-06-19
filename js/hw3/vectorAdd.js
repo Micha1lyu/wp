ad183e683810d9fd8d2e10219343c9caed329088
Micha1lyu <144764406+Micha1lyu@users.noreply.github.com>
2024-06-20 02:15:49 +0800

    Create vectorAdd.js

---
+++ js/hw3/vectorAdd.js
@@ -0,0 +1,9 @@
+function vectorAdd(a, b) {
+    var c = [];
+    for (let i = 0;i < a.length;i ++) {
+        c[i] = a[i] + b[i];
+    }
+    console.log(c);
+}
+var a=[1,2],b=[5,6]
+console.log(vectorAdd(a,b))
