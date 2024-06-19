78cc934f4bb71fae3f55da00e92360ddb5920c4f
Micha1lyu <144764406+Micha1lyu@users.noreply.github.com>
2024-06-20 01:28:05 +0800

    Update sumPrime.js

--- js/hw3/sumPrime.js
+++ js/hw3/sumPrime.js
@@ -1,1 +1,17 @@
+function isprime(n) {
+    for (let i = 2;i < n;i ++){
+        if (n % i == 0) return false
+    }
+    return true
+}
 
+let a=0
+function sumprime(n){
+    for(let i = 2;i <= n;i ++){
+        if (isprime(i)){
+            a += i;
+        }
+    }
+}
+sumprime(7)
+console.log(a)
