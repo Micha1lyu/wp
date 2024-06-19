f1dd4b36dd1db6ed67c58d83cf3255919da9c0e6
Micha1lyu <144764406+Micha1lyu@users.noreply.github.com>
2024-06-20 02:24:41 +0800

    Update matrixAdd.js

--- js/hw3/matrixAdd.js
+++ js/hw3/matrixAdd.js
@@ -1,1 +1,14 @@
-
+function matrixadd(a,b){
+    var i,j,c = []
+    for(i = 0;i < a.length;i ++)
+    {
+        c[i] = []
+        for(j = 0;j < a[i].length;j ++)
+        {
+            c[i][j] = a[i][j] +b[i][j]
+        }
+    }
+    return c
+}
+var a = [[1,2],[3,4]],b = [[5,6],[7,8]]
+console.log(matrixadd(a,b))
