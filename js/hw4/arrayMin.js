ec5af3b53de239af0d4a9824500ff484b1ade89e
Micha1lyu <144764406+Micha1lyu@users.noreply.github.com>
2024-06-22 13:40:30 +0800

    Update arrayMin.js

--- js/hw4/arrayMin.js
+++ js/hw4/arrayMin.js
@@ -1,13 +1,13 @@
 function arrayMin(a)
 {
-    var min = a[0];
+    var min = a[0]
     for(let i = 1;i < a.length;i ++){
         if(min > a[i]){
-            min = a[i];
+            min = a[i]
         }
     }
-    return min;
+    return min
 }
 
-var a = [4, 3, 7, 5, 11, 9];
-console.log("arrayMin is=", arrayMin(a));
+var a = [4, 3, 7, 5, 11, 9]
+console.log("arrayMin is=", arrayMin(a))
