e02a1279f90a462f319c7b8820a6ebef80dc2f18
Micha1lyu <144764406+Micha1lyu@users.noreply.github.com>
2024-06-22 14:12:14 +0800

    Update weekday.js

--- js/hw4/weekday.js
+++ js/hw4/weekday.js
@@ -1,1 +1,14 @@
-
+var d2i = {
+    Sunday:0,
+    Monday:1,
+    Tuesday:2,
+    Wednesday:3,
+    Thursday:4,
+    Friday:5,
+    Saturday:6,
+}
+function weekday(day){
+    return d2i[day]
+}
+let day = Deno.args[0]
+console.log(day, '=', weekday(day))
