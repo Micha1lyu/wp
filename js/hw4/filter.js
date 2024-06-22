8d537f16a87244ee7b1e23370327e1bef1bc756f
Micha1lyu <144764406+Micha1lyu@users.noreply.github.com>
2024-06-22 14:01:06 +0800

    Update filter.js

--- js/hw4/filter.js
+++ js/hw4/filter.js
@@ -1,1 +1,13 @@
+function filter(a, f){
+    var result = []
+    for (let i = 0;i < a.length;i ++){
+        if (f(a[i])){
+            result.push(a[i])
+        }
+    }
+    return result;
+}
+console.log(filter([1, 2, 3, 4], function(x){ return x % 2 == 1; }))
+console.log(filter([10, 15, 20, 25], function(x){ return x > 15; }))
+
 
