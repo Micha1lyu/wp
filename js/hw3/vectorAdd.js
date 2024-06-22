9614e5690679c27eda343b2a4bca9449098127ca
Micha1lyu <144764406+Micha1lyu@users.noreply.github.com>
2024-06-22 13:42:29 +0800

    Update vectorAdd.js

--- js/hw3/vectorAdd.js
+++ js/hw3/vectorAdd.js
@@ -1,9 +1,9 @@
-function vectorAdd(a, b) {
-    var c = [];
-    for (let i = 0;i < a.length;i ++) {
-        c[i] = a[i] + b[i];
+function vectorAdd(a, b){
+    var c = []
+    for (let i = 0;i < a.length;i ++){
+        c[i] = a[i] + b[i]
     }
-    console.log(c);
+    console.log(c)
 }
 var a = [1,2],b = [5,6]
 console.log(vectorAdd(a,b))
