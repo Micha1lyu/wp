c76302d7d529abc39c6dd9763992100bac144051
Micha1lyu <144764406+Micha1lyu@users.noreply.github.com>
2024-06-22 14:31:56 +0800

    Update countChar.js

--- js/hw4/countChar.js
+++ js/hw4/countChar.js
@@ -1,1 +1,14 @@
+function countChar(str) {
+    var result = {};
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (result[char]) {
+            result[char]++;
+        } else {
+            result[char] = 1;
+        }
+    }
+    return result;
+}
 
+console.log(countChar("aabcedeaac")); 
